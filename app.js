6cf8892a849e1b917a100b89bfcf673125c15e3f
Nelson R. Perez <bilthon@gmail.com>
2015-06-26 21:13:28 -0500

    Filtering people

--- app.js
+++ app.js
@@ -15,84 +15,66 @@ var MAX_ELAPSED_TIME = 1000*60*60*24;
 
 var pagesCounter = 1;
 
+var person = {};
+
 var requestPage = function(){
 
 }
 
 var buffer;
+iconv.extendNodeEncodings(); 
 
+function char_convert(str_to_convert){
+    var chars = ["©","Û","®","ž","Ü","Ÿ","Ý","$","Þ","%","¡","ß","¢","à","£","á","À","¤","â","Á","¥","ã","Â","¦","ä","Ã","§","å","Ä","¨","æ","Å","©","ç","Æ","ª","è","Ç","«","é","È","¬","ê","É","­","ë","Ê","®","ì","Ë","¯","í","Ì","°","î","Í","±","ï","Î","²","ð","Ï","³","ñ","Ð","´","ò","Ñ","µ","ó","Õ","¶","ô","Ö","·","õ","Ø","¸","ö","Ù","¹","÷","Ú","º","ø","Û","»","ù","Ü","@","¼","ú","Ý","½","û","Þ","€","¾","ü","ß","¿","ý","à","‚","À","þ","á","ƒ","Á","ÿ","å","„","Â","æ","…","Ã","ç","†","Ä","è","‡","Å","é","ˆ","Æ","ê","‰","Ç","ë","Š","È","ì","‹","É","í","Œ","Ê","î","Ë","ï","Ž","Ì","ð","Í","ñ","Î","ò","‘","Ï","ó","’","Ð","ô","“","Ñ","õ","”","Ò","ö","•","Ó","ø","–","Ô","ù","—","Õ","ú","˜","Ö","û","™","×","ý","š","Ø","þ","›","Ù","ÿ","œ","Ú"]; 
+    var codes = ["&copy;","&#219;","&reg;","&#158;","&#220;","&#159;","&#221;","&#36;","&#222;","&#37;","&#161;","&#223;","&#162;","&#224;","&#163;","&#225;","&Agrave;","&#164;","&#226;","&Aacute;","&#165;","&#227;","&Acirc;","&#166;","&#228;","&Atilde;","&#167;","&#229;","&Auml;","&#168;","&#230;","&Aring;","&#169;","&#231;","&AElig;","&#170;","&#232;","&Ccedil;","&#171;","&#233;","&Egrave;","&#172;","&#234;","&Eacute;","&#173;","&#235;","&Ecirc;","&#174;","&#236;","&Euml;","&#175;","&#237;","&Igrave;","&#176;","&#238;","&Iacute;","&#177;","&#239;","&Icirc;","&#178;","&#240;","&Iuml;","&#179;","&#241;","&ETH;","&#180;","&#242;","&Ntilde;","&#181;","&#243;","&Otilde;","&#182;","&#244;","&Ouml;","&#183;","&#245;","&Oslash;","&#184;","&#246;","&Ugrave;","&#185;","&#247;","&Uacute;","&#186;","&#248;","&Ucirc;","&#187;","&#249;","&Uuml;","&#64;","&#188;","&#250;","&Yacute;","&#189;","&#251;","&THORN;","&#128;","&#190;","&#252","&szlig;","&#191;","&#253;","&agrave;","&#130;","&#192;","&#254;","&aacute;","&#131;","&#193;","&#255;","&aring;","&#132;","&#194;","&aelig;","&#133;","&#195;","&ccedil;","&#134;","&#196;","&egrave;","&#135;","&#197;","&eacute;","&#136;","&#198;","&ecirc;","&#137;","&#199;","&euml;","&#138;","&#200;","&igrave;","&#139;","&#201;","&iacute;","&#140;","&#202;","&icirc;","&#203;","&iuml;","&#142;","&#204;","&eth;","&#205;","&ntilde;","&#206;","&ograve;","&#145;","&#207;","&oacute;","&#146;","&#208;","&ocirc;","&#147;","&#209;","&otilde;","&#148;","&#210;","&ouml;","&#149;","&#211;","&oslash;","&#150;","&#212;","&ugrave;","&#151;","&#213;","&uacute;","&#152;","&#214;","&ucirc;","&#153;","&#215;","&yacute;","&#154;","&#216;","&thorn;","&#155;","&#217;","&yuml;","&#156;","&#218;"];
+    var result = str_to_convert;
+	for(i=0; i < codes.length; i++){
+		result = result.replace(codes[i],chars[i])
+	}
+	return result;
+}
 /*
 * Function used to download the data in case it is not already loaded
 */
 var downloadData = function(callback){
 	console.log('downloading data..');
 
-	// var options = {
-	// 	uri: 'http://'+IPS_HOST+IPS_PATH+pagesCounter,
-	// 	method: 'GET',
-	// 	encoding: null
-	// };
+	var options = {
+		uri: 'http://'+IPS_HOST+IPS_PATH+pagesCounter,
+		method: 'GET',
+		encoding: null
+	};
 
-	// request(options, function (error, response, body) {
- //  		if (!error && response.statusCode == 200) {
- //  			console.log('Got page number: '+pagesCounter);
-	// 		// var fd = fs.openSync(data_file, 'a');
-	// 		// var ic = new iconv.Iconv('iso-8859-1', 'utf-8');
-	// 		// var buf = ic.convert(body);
-	// 		str = iconv.decode(new Buffer(body), "ISO-8859-1");
-	// 		// fs.writeSync(fd, buf.toString('utf-8'));
-	// 		// fs.writeSync(fd, buffer.toString('utf8'), 0, 'utf8');
-
-	// 		fs.writeFileSync(data_file, iconv.encode(str, 'utf8'));
-	// 		// fs.closeSync(fd);
-	// 		pagesCounter = pagesCounter + 1;
-	// 		if(pagesCounter > 1)
-	// 			// callback();
-	// 			console.log('finish');
-	// 		else
-	// 			downloadData(callback);
-
- //  		}else{
- //  			console.log('error. code: '+response.statusCode);
- //  		}
-	// })
-
-	var req = http.request('http://'+IPS_HOST+IPS_PATH+pagesCounter, function(res){
-		var data = new Buffer(0);
-		res.on('data', function(chunk){
-			data = Buffer.concat([data, chunk]);
-		})
-		res.on('end', function(){
-			var fd = fs.openSync(data_file, 'a');
-			// var ic = new iconv.Iconv('iso-8859-1', 'utf-8');
-			// var buf = ic.convert(data);
-			str = iconv.decode(data, "ISO-8859-1");
-			console.log('end. str length: '+str.length);
-			fs.writeSync(fd, str);
-			fs.closeSync(fd);
+	request(options, function (error, response, body) {
+  		if (!error && response.statusCode == 200) {
+  			console.log('Got page number: '+pagesCounter);
+			var str = iconv.decode(body, "iso-8859-1");
+			var converted = char_convert(str);
+			fs.writeFileSync(data_file, converted, {flag:'a'});
 			pagesCounter = pagesCounter + 1;
 			if(pagesCounter > IPS_PAGES)
 				callback();
 			else
 				downloadData(callback);
-		})
-	});
-	req.end();
+
+  		}else{
+  			console.log('error. code: '+response.statusCode);
+  		}
+	})
 }
 
 /* Function used to parse the file */
 var parseData = function(){
 	var KEYS = [];
 	var people = [];
-	var data = fs.readFileSync(data_file);
-	var promise = htmlToJson.parse(data.toString('utf-8'), {
-	    'text':function($doc, $){
+	var data = fs.readFileSync(data_file).toString('utf8')
+	var promise = htmlToJson.parse(data, {
+	    'content':function($doc, $){
 	    	$('.datagrid').find('thead').children().children().each(function(index, element){
 	    		if(KEYS.length < COLUMN_COUNT){
 	    			KEYS.push($(this).contents());
 	    		}
 	    	});
-			var person = {};
 			var skipped = 0;
 			var line = 0;
 			var column = 0;
@@ -110,6 +92,7 @@ var parseData = function(){
 					if(column+1 == KEYS.length){
 						// console.log('pushing new person. line: '+line+', column: '+column+', counter: '+counter+', f: '+f+', people length: '+people.length+', name: '+person[KEYS[1]]);
 						people.push(person);
+						console.log(JSON.stringify(person));
 						person = {};
 					}
 				}else{
@@ -130,7 +113,7 @@ var parseData = function(){
 
 	promise.done(function(res){
 		console.log('promise done. result: '+res);
-		console.log('result length: '+res.length);
+		console.log('result length: '+res.contents);
 	});
 }
 
